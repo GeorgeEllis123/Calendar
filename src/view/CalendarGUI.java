46eb310b81ab2cbc90af7027b1db3a6aeeaff54f
george <70286749+GeorgeEllis123@users.noreply.github.com>
2025-06-15 23:46:40 -0400

    Added gui interface

---
+++ src/view/CalendarGUI.java
@@ -0,0 +1,38 @@
+package view;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import model.IEvent;
+
+public interface CalendarGUI {
+
+  /**
+   * Returns the requested day get information from.
+   *
+   * @return the requested day to load
+   */
+  public LocalDate getLoadDay();
+
+  /**
+   * Sends all the information the user inputted when trying to create an event.
+   *
+   * @return the information the user inputed [0] is subject [1] is start [1] is end
+   */
+  public String[] getCreate();
+
+  /**
+   * Loads all events on the current day in the calendar.
+   *
+   * @param events the events to display in the calendar
+   */
+  public void loadDay(ArrayList<IEvent> events);
+
+  /**
+   * Displays an error message to the user.
+   *
+   * @param msg the msg to display
+   */
+  public void displayError(String msg);
+
+}
---
+++ src/view/CalendarGUIImpl.java
@@ -0,0 +1,106 @@
+package view;
+
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.*;
+
+import model.IEvent;
+
+public class CalendarGUIImpl implements CalendarGUI {
+
+  private JFrame frame;
+  private JPanel calendarPanel;
+  private JLabel monthLabel;
+  private JComboBox<String> calendarDropdown;
+  private Map<String, Color> calendars;
+  private Map<LocalDate, List<String>> events;
+  private YearMonth currentMonth;
+  private String selectedCalendar;
+
+  public CalendarGUIImpl() {
+    frame = new JFrame("Calendar App");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(500, 500);
+    frame.setLayout(new BorderLayout());
+
+    currentMonth = YearMonth.now();
+    calendars = new HashMap<>();
+    events = new HashMap<>();
+    calendars.put("Work", Color.BLUE);
+    calendars.put("Personal", Color.GREEN);
+    calendars.put("Holidays", Color.RED);
+    selectedCalendar = "Work";
+
+    JPanel topPanel = new JPanel();
+    JButton prevButton = new JButton("<");
+    JButton nextButton = new JButton(">");
+    monthLabel = new JLabel();
+    calendarDropdown = new JComboBox<>(calendars.keySet().toArray(new String[0]));
+    topPanel.add(prevButton);
+    topPanel.add(monthLabel);
+    topPanel.add(nextButton);
+    topPanel.add(calendarDropdown);
+
+    frame.add(topPanel, BorderLayout.NORTH);
+
+    calendarPanel = new JPanel();
+    frame.add(calendarPanel, BorderLayout.CENTER);
+
+    updateCalendar();
+    frame.setVisible(true);
+  }
+
+  private void updateCalendar() {
+    calendarPanel.removeAll();
+    calendarPanel.setLayout(new GridLayout(0, 7));
+    monthLabel.setText(currentMonth.getMonth() + " " + currentMonth.getYear());
+    calendarPanel.setBackground(calendars.get(selectedCalendar));
+
+    for (int day = 1; day <= currentMonth.lengthOfMonth(); day++) {
+      LocalDate date = currentMonth.atDay(day);
+      JButton dayButton = new JButton(String.valueOf(day));
+      calendarPanel.add(dayButton);
+    }
+
+    frame.revalidate();
+    frame.repaint();
+  }
+
+  @Override
+  public LocalDate getLoadDay() {
+    return null;
+  }
+
+  @Override
+  public String[] getCreate() {
+    return new String[0];
+  }
+
+  @Override
+  public void loadDay(ArrayList<IEvent> events) {
+
+  }
+
+  @Override
+  public void displayError(String msg) {
+    
+  }
+
+  public static void main(String[] args) {
+    /**
+     * Runs the GUI asynchronously without blocking.
+     */
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        new CalendarGUIImpl();
+      }
+    });
+  }
+}
